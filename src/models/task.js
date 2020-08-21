b7b02411877d91056a9182b32a213ea05268f729
ap0109 <amitporia132@gmail.com>
2020-08-21 18:41:58 +0930

    added authentication on task services

--- src/models/task.js
+++ src/models/task.js
@@ -11,6 +11,11 @@ const Tasks = mongoose.model('Task',{
         type:Boolean,
         default:false
 
+    },
+    owner : {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
 })
 
