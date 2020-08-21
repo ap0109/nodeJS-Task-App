c84fd9110776fae3b32ded723623a2da5bbf9e28
ap0109 <amitporia132@gmail.com>
2020-08-21 10:03:24 +0930

    added authentication on user services

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port =  process.env.port || 3000
 
-app.use((req, res,next ) => {
-    res.status(503).send('Site is under maintainence mode. Please try after some time!')
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -18,3 +14,17 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on prt '+port);
 }) 
+
+const jwt = require('jsonwebtoken')
+
+const myFunction = async () => {
+
+    const token = jwt.sign({ _id:'abc123'}, 'thisismynewcourse', {expiresIn:'7 days'})
+    console.log(token)
+
+    const data =  jwt.verify(token, 'thisismynewcourse')
+    console.log(data)
+
+}
+
+myFunction()
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth =  require('../middleware/authentication')
 
 const router =  new express.Router()
 
@@ -26,30 +27,56 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout', auth,  async (req, res) => {
     try {
-        const users = await User.find({})
-        res.send(users)
+        req.user.tokens =  req.user.tokens.filter((token) => {
+            return token.token != req.token
+        })
+
+        await req.user.save()
+
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+    try {
+        //const users = await User.find({})
+        res.send(req.user)
     } catch (error) {
         res.status(500).send(error)
     }
 })
 
-router.get('/users/:id', async (req, res) => {
-    try {
-        const _id = req.params.id;
-        const user =  await User.findById(_id)
-        if(!user){
-            res.status(404).send()
-        }
-        res.send(user)
-    } catch (error) {
-        res.status(500).send()
+//This method os of no use - We are not going to fetch user details with it's _id - 
+//because we have already all the details for authentication layer
+// router.get('/users/:id', async (req, res) => {
+//     try {
+//         const _id = req.params.id;
+//         const user =  await User.findById(_id)
+//         if(!user){
+//             res.status(404).send()
+//         }
+//         res.send(user)
+//     } catch (error) {
+//         res.status(500).send()
         
-    }
-})
+//     }
+// })
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me',auth, async (req, res) => {
     try {
         const updates =  Object.keys(req.body)
         const allowUpdates = ['name', 'email', 'age', 'password']
@@ -65,29 +92,22 @@ router.patch('/users/:id', async (req, res) => {
         // But as this is update call - we need  trick this - so that we can call save method
         //const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
 
-        const user =  await User.findById(req.params.id)
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save()
+        //const user =  await User.findById(req.params.id)
 
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
 
-        if(!user){
-            res.status(404).send()
-        }
-        res.send(user)
+        res.send(req.user)
     } catch (error) {
         res.status(500).send(error)
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
-
+router.delete('/users/me', auth , async (req, res) => {
     try {
-        const user =  await User.findByIdAndDelete(req.params.id)
-
-        if(!user){
-            res.status(404).send()
-        }
-        res.send(user)
+        //const user =  await User.findByIdAndDelete(req.params.id)
+        await req.user.remove()
+        res.send(req.user)
     } catch (error) {
         res.status(500).send(error)
     }
