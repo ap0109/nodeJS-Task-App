3e4c4cbe93292f010342adfe48ef6652e6508cb6
ap0109 <amitporia132@gmail.com>
2020-08-25 23:26:12 +0930

    Sending Emails and setting up environment variables

--- src/index.js
+++ src/index.js
@@ -5,54 +5,13 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port =  process.env.port || 3000
+const port =  process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 
-const multer =  require('multer')
-
-const upload = multer({
-    dest: 'images',
-    limits:1000000,
-    fileFilter (req, file, callback) {
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return callback(new Error('Please upload a word document'))
-        }
-        callback(undefined, true)
-    }
-})
-
-const errorMiddleware = (req, res, next) => {
-    throw new Error('From my middleware')
-}
-
-app.post('/upload', errorMiddleware ,(req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({error: error.message})
-})
-
-// app.post('/upload', upload.single('upload') ,(req, res) => {
-//     res.send()
-// })
-
 app.listen(port, () => {
     console.log('Server is up on prt '+port);
 }) 
-
-// const jwt = require('jsonwebtoken')
-
-// const myFunction = async () => {
-
-//     const token = jwt.sign({ _id:'abc123'}, 'thisismynewcourse', {expiresIn:'7 days'})
-//     console.log(token)
-
-//     const data =  jwt.verify(token, 'thisismynewcourse')
-//     console.log(data)
-
-// }
-
-// myFunction()
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const multer =  require('multer')
 const sharp =  require('sharp')
 const User = require('../models/user')
 const auth =  require('../middleware/authentication')
+const { sendWelcomeEmail , sendCancellationEmail} =  require('../emails/accounts')
 
 const router =  new express.Router()
 
@@ -11,6 +12,7 @@ router.post('/users', async (req, res) => {
     
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
 
         res.status(201).send({user:user, token:token})
@@ -109,6 +111,7 @@ router.delete('/users/me', auth , async (req, res) => {
     try {
         //const user =  await User.findByIdAndDelete(req.params.id)
         await req.user.remove()
+        sendCancellationEmail(user.email, user.name)
         res.send(req.user)
     } catch (error) {
         res.status(500).send(error)
